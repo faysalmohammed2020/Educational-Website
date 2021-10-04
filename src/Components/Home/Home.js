e317924b98ccc47d73700466acb83fda5cf08cc9
Your Name <you@example.com>
2021-10-04 13:02:25 +0600

    updated

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const Home = () => {
+    return (
+        const(course,setCourse) = useState([])
+        <div>
+            
+        </div>
+    );
+};
+
+export default Home;
